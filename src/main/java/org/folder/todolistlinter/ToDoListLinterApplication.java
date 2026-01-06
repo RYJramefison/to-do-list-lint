19014b515018f624dfb22838afa3bdd1daf2acc1
juninho <hei.juninho.2@gmail.com>
2026-01-06 15:18:29 +0300

    Initial commit

---
+++ src/main/java/org/folder/todolistlinter/ToDoListLinterApplication.java
@@ -0,0 +1,13 @@
+package org.folder.todolistlinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListLinterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListLinterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/folder/todolistlinter/ToDoListLinterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.folder.todolistlinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListLinterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
